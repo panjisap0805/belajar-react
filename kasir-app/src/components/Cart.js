c8514995e9edb2b8b8ea75ea35b20a2216bcb920
panjisap0805 <p@Nji_16>
2021-01-13 17:16:51 +0700

    Menampilkan produk

---
+++ kasir-app/src/components/Cart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Col } from 'react-bootstrap'
+
+export default class Cart extends Component {
+    render() {
+        return (
+            <Col md={3} mt="2">
+                <h4><strong>Cart</strong></h4>
+                <hr />
+            </Col>
+        )
+    }
+}
---
+++ kasir-app/src/components/index.js
@@ -0,0 +1,6 @@
+import Cart from './Cart'
+import ListCategories from './ListCategories'
+import NavbarComponent from './NavbarComponent'
+import Menus from './Menus'
+
+export { Cart, ListCategories, NavbarComponent, Menus }
