c98de7f11c823cfd14c1caecd702bb113ab8c5a2
panjisap0805 <p@Nji_16>
2021-01-14 12:40:30 +0700

    Categories

--- kasir-app/src/App.js
+++ kasir-app/src/App.js
@@ -11,13 +11,30 @@ export default class App extends Component {
   
     this.state = {
       menus: [],
-
+      clickedCategory: "Makanan",
     }
   }
 
   componentDidMount() {
     axios
-      .get(API_URL+"products")
+      .get(API_URL+"products?category.nama="+this.state.clickedCategory)
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  changeCategory = (value) => {
+    this.setState({
+      clickedCategory: value,
+      menus: []
+    })
+
+    axios
+      .get(API_URL+"products?category.nama="+value)
       .then(res => {
         const menus = res.data;
         this.setState({ menus });
@@ -28,14 +45,14 @@ export default class App extends Component {
   }
   
   render() {
-    const { menus } = this.state
+    const { menus, clickedCategory } = this.state
     return (
       <div className="App">
       <NavbarComponent />
       <div className="mt-3">
         <Container fluid>
           <Row>
-            <ListCategories />
+            <ListCategories changeCategory={this.changeCategory} clickedCategory={clickedCategory}/>
             <Col>
               <h4><strong>Daftar Product</strong></h4>
               <hr />
